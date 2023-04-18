2d527b4313cd6a91524423d8b8c2f4d9c8024f65
Bedirhan <albayrakbedirhan570@gmail.com>
2023-04-18 07:43:23 +0300

    Initial commit

---
+++ src/asall.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class asall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
+
+        if (asalMi(sayi, 2)) {
+            System.out.println(sayi + " asal bir sayıdır.");
+        } else {
+            System.out.println(sayi + " asal bir sayı değildir.");
+        }
+    }
+
+    public static boolean asalMi(int sayi, int bolen) {
+        if (sayi <= 2) {
+            return (sayi == 2);
+        }
+        if (sayi % bolen == 0) {
+            return false;
+        }
+        if (bolen * bolen > sayi) {
+            return true;
+        }
+        return asalMi(sayi, bolen + 1);
+    }
+}
